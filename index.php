c10785edebb4c78c1d08fe6e395fb5f86b57403b
Pierre Rudloff <contact@rudloff.pro>
2013-01-21 10:37:42 +0000

    Adaptation du CSS

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-function output ($string) {
+//Config
+//define('COMPRESS_IMAGES', true);
+
+function output ($string)
+{
     //print(gzencode($string));
     print(($string));
 }
+
+function removeCSSProp($prop, $css)
+{
+    return preg_replace('/\s*'.$prop.':.*;/', '', $css);
+}
+
 header_remove('Content-Type');
 $url=$_SERVER['REQUEST_URI'];
 if ($url!='/') {
@@ -11,18 +21,18 @@ if ($url!='/') {
     if (is_array($contentType)) {
         $contentType=$contentType[0];
     }
-    $contentType=explode(';',$contentType);
+    $contentType=explode(';', $contentType);
     header('Content-Type: '.$contentType[0]);
     ini_set('user_agent', $_SERVER['HTTP_USER_AGENT']);
     //Cache
     header('Cache-Control: max-age=2678400');
     header('Vary: Accept-Encoding');
-    if(explode('/', $contentType[0])[0]=='image') {
-        if($contentType[0]=='image/jpeg') {
+    if (defined('COMPRESS_IMAGES') && explode('/', $contentType[0])[0]=='image') {
+        if ($contentType[0]=='image/jpeg') {
             $image = imagecreatefromjpeg($url);
-        } else if($contentType[0]=='image/gif') {
+        } else if ($contentType[0]=='image/gif') {
             $image = imagecreatefromgif($url);
-        } else if($contentType[0]=='image/png') {
+        } else if ($contentType[0]=='image/png') {
             $image = imagecreatefrompng($url);
         }
         imagejpeg($image, null, 50);
@@ -30,7 +40,7 @@ if ($url!='/') {
         //header('Content-Encoding: gzip');
         $content=file_get_contents($_SERVER['REQUEST_URI']);
         header('ETag: '.md5($content));
-        if($contentType[0]=='text/html') {
+        if ($contentType[0]=='text/html') {
             $dom=new DOMDocument();
             $dom->preserveWhiteSpace=false;
             $dom->formatOutput=false;
@@ -41,10 +51,21 @@ if ($url!='/') {
             @$dom->loadHTML($content);
             $styles=$dom->getElementsByTagName('script');
             for ($i=0;$i<$styles->length;$i++) {
-                
+                $styles->item($i)
+                    ->nodeValue=removeCSSProp('float', $styles->item($i)->nodeValue);
+                $styles->item($i)
+                    ->nodeValue=removeCSSProp('width', $styles->item($i)->nodeValue);
             }
             output($dom->saveHTML());
-        } else if ($contentType[0]=='text/javascript' || $contentType[0]=='text/css') {
+        } else if ($contentType[0]=='text/javascript'
+            || $contentType[0]=='text/css'
+        ) {
+            if ($contentType[0]=='text/css') {
+                $content=removeCSSProp('float', $content);
+                $content=removeCSSProp('width', $content);
+                $content=removeCSSProp('margin\-\w+', $content);
+                $content=removeCSSProp('background-image', $content);
+            }
             output(preg_replace('/\v/', '', $content));
         } else {
             output($content);
